dd1c3772addbdb37127a5aaa817ca2d9e78fe024
Terence PERSIN <terence.persin@gerflor.com>
2021-12-03 10:23:05 +0100

    NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,13 +7,13 @@ export default function NavBar() {
     return (
         <>
             <Navbar bg="light" expand="lg">
-                <Nav.Item>
-                    <Link to="/"><img src={logo} className="NavBar-logo" alt="logo" /></Link>
-                </Nav.Item>
                 <Container>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="me-auto d-flex align-items-center">
+                        <Nav.Item style={{marginRight: 15}}>
+                            <Link to="/"><img src={logo} className="NavBar-logo" alt="logo" /></Link>
+                        </Nav.Item>
                         <Nav.Item style={{marginRight: 15}}>
                             <Link to="/">Home</Link>
                         </Nav.Item>
