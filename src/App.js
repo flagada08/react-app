6eaf7d1ac195326a601bdd76085e349c43a3eb3f
Terence PERSIN <terence.persin@gerflor.com>
2021-12-08 13:27:37 +0100

    bootstraped NavBar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
         <Route path="/profile" element={
           <RequireToken>
             <Profile />
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import {Nav, Navbar, Container} from 'react-bootstrap';
-import {Link} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 import logo from "./logo.svg";
 
 export default function NavBar() {
@@ -7,21 +7,21 @@ export default function NavBar() {
     return (
         <>
             <Navbar bg="light" expand="lg">
-                <Container>
+                <Container style={{margin: 0}}>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto d-flex align-items-center">
-                        <Nav.Item style={{marginRight: 15}}>
-                            <Link to="/"><img src={logo} className="NavBar-logo" alt="logo" /></Link>
+                    <Nav.Item>
+                        <Link to="/"><img src={logo} className="NavBar-logo" alt="logo" /></Link>
+                    </Nav.Item>
+                    <Nav variant="tabs" defaultActiveKey="/">
+                        <Nav.Item>
+                            <NavLink className="nav-link" to="/">Home</NavLink>
                         </Nav.Item>
-                        <Nav.Item style={{marginRight: 15}}>
-                            <Link to="/">Home</Link>
+                        <Nav.Item>
+                            <NavLink className="nav-link" to="/about">About</NavLink>
                         </Nav.Item>
-                        <Nav.Item style={{marginRight: 15}}>
-                            <Link to="/about">About</Link>
-                        </Nav.Item>
-                        <Nav.Item style={{marginRight: 15}}>                            
-                            <Link to="/profile">Profile</Link>                            
+                        <Nav.Item>                            
+                            <NavLink className="nav-link" to="/profile">Profile</NavLink>
                         </Nav.Item>
                     </Nav>
                     </Navbar.Collapse>
