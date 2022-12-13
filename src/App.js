71f4d1968aeddc8a9f942360effbdfe523022860
TERENCE <flagada08@gmail.com>
2022-12-13 16:27:53 +0100

    _redirects file

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/loginapp" element={<Home />} />
-        <Route path="/loginapp/about" element={<About />} />
-        <Route path="/loginapp/profile" element={
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={
           <RequireToken>
             <Profile />
           </RequireToken>
